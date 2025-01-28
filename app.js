5e5e1cfd97217f8a0820b4e62fd76102bfe90531
Esteban-Alarcon <esteban94al@hotmail.com>
2025-01-27 22:41:57 -0600

    Proyecto Final Lógica de programación
    Proyecto final curso Lógica de programación (Explorar funciones y listas). Consta de un videojuego sobre adivinar un número secreto generado pseudo-aleatoriamente por la máquina.

---
+++ app.js
@@ -0,0 +1,68 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    if (numeroDeUsuario === numeroSecreto){
+        //El usuario acertó
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}`);
+        document.querySelector('#reiniciar').removeAttribute('disabled');
+    } else{
+        //El usuario no acertó aún
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        } else{
+            asignarTextoElemento('p', 'El número secreto es menor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+function limpiarCaja(){
+    let valorCaja= document.querySelector('#valorUsuario').value='';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+    //Si ya sorteamos todos los números, cerrar el juego
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles, por favor refresca la pantalla con F5');
+    }
+    else{
+    //Si el número generado está incluído en la lista
+    if (listaNumerosSorteados.includes(numeroGenerado)){
+        return generarNumeroSecreto;
+    } else{
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
+    }
+}
+
+
+function reiniciarJuego(){
+    //limpiar la caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números
+    //Generar el número aleatorio
+    //Inicializar el número de intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disable','true');
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', `Indica el número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+condicionesIniciales();
